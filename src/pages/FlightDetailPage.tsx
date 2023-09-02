842cb76a7dce63e5d66461836e3fd45b389565f0
Mudassir742 <mudassirwaheed242@gmail.com>
2023-09-02 16:56:16 +0500

    flight detail card designs completed

--- src/pages/FlightDetailPage.tsx
+++ src/pages/FlightDetailPage.tsx
@@ -72,6 +72,93 @@ const FlightDetailPage = () => {
             </div>
           </div>
         </div>
+        <div className="headerCard" style={{ margin: "2rem 0" }}>
+          <div className="flighCompleteDetail">
+            <div className="flightDetailContent">
+              <div className="flightNameContainer">
+                <Avatar size={45}>K</Avatar>
+                <h1 className="flightName">Indigo 7442</h1>
+              </div>
+              <div className="flightTimeDetail">
+                <div className="flightTime">
+                  <h3>12:45</h3>
+                </div>
+                <div className="cityDivider">
+                  <span className="circle circle1"></span>
+                  <div className="divider"></div>
+                  <span className="circle circle2"></span>
+                </div>
+                <div className="flightTime">
+                  <h3>15:30</h3>
+                </div>
+              </div>
+              <div className="flightCity">
+                <p>Delhi</p>
+                <p>2h 25m - 1 Stop</p>
+                <p>Mumbai</p>
+              </div>
+              <div className="chipsSection">
+                <div className="chip">
+                  <p>Economy</p>
+                </div>
+                <div className="chip">
+                  <p>24-08-2023</p>
+                </div>
+                <div className="chip">
+                  <p>Cabin Baggage 7 KG</p>
+                </div>
+                <div className="chip">
+                  <p>Checkin Baggage 8 KG</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mainDivider">
+              <span className="circle circle1"></span>
+              <div className="divider"></div>
+              <span className="circle circle2"></span>
+            </div>
+
+            <div className="flightDetailContent">
+              <div className="flightNameContainer">
+                <Avatar size={45}>K</Avatar>
+                <h1 className="flightName">Indigo 7442</h1>
+              </div>
+              <div className="flightTimeDetail">
+                <div className="flightTime">
+                  <h3>12:45</h3>
+                </div>
+                <div className="cityDivider">
+                  <span className="circle circle1"></span>
+                  <div className="divider"></div>
+                  <span className="circle circle2"></span>
+                </div>
+                <div className="flightTime">
+                  <h3>15:30</h3>
+                </div>
+              </div>
+              <div className="flightCity">
+                <p>Delhi</p>
+                <p>2h 25m - 1 Stop</p>
+                <p>Mumbai</p>
+              </div>
+              <div className="chipsSection">
+                <div className="chip">
+                  <p>Economy</p>
+                </div>
+                <div className="chip">
+                  <p>24-08-2023</p>
+                </div>
+                <div className="chip">
+                  <p>Cabin Baggage 7 KG</p>
+                </div>
+                <div className="chip">
+                  <p>Checkin Baggage 8 KG</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
