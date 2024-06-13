2390c483988a7727c37b5a37e1e9086b8c26424d
Shreyas Kyada <83200751+shreyaskyada@users.noreply.github.com>
2024-06-13 09:46:46 +0530

    remove port

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Allow access from the network
-    port: 3000, // Specify a port if needed
-    strictPort: true, // Ensure the server uses the specified port, or fails if it's not available
     open: false, // Do not open the browser automatically
   },
   build: {
