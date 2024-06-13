82dad85676562c01f81d6cd0ba9d2a0d5ca1df5a
Shreyas Kyada <83200751+shreyaskyada@users.noreply.github.com>
2024-06-13 10:09:04 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true, // Allow access from the network
+    open: false, // Do not open the browser automatically
+  },
+  build: {
+    minify: true, // Use default minification
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Use default chunking
+      },
+    },
+  },
 });
