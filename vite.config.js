2deac67af14c7282ec30b71995f17d56aa9bebaf
Shreyas Kyada <83200751+shreyaskyada@users.noreply.github.com>
2024-06-13 09:41:27 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true, // Allow access from the network
+    port: 3000, // Specify a port if needed
+    strictPort: true, // Ensure the server uses the specified port, or fails if it's not available
+    open: false, // Do not open the browser automatically
+  },
+  build: {
+    minify: true, // Use default minification
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Use default chunking
+      },
+    },
+  },
 });
